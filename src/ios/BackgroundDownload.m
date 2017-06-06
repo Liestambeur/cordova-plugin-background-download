9d0a782818c1256a4c83662d33c4d2f20ceec47b
lies tambeur <lies.tambeur@twipemobile.com>
2017-06-06 14:49:56 +0200

    Cookie iOS

--- src/ios/BackgroundDownload.m
+++ src/ios/BackgroundDownload.m
@@ -23,10 +23,107 @@
 
 @implementation BackgroundDownload
 
+- (NSDictionary *)cookieMap:(NSString*)cookie{
+    NSMutableDictionary *cookieMap = [NSMutableDictionary dictionary];
+
+    NSArray *cookieKeyValueStrings = [cookie componentsSeparatedByString:@";"];
+    for (NSString *cookieKeyValueString in cookieKeyValueStrings) {
+        NSRange separatorRange = [cookieKeyValueString rangeOfString:@"="];
+
+        if (separatorRange.location != NSNotFound &&
+            separatorRange.location > 0 &&
+            separatorRange.location < ([cookieKeyValueString length] - 1)) {
+
+            NSRange keyRange = NSMakeRange(0, separatorRange.location);
+            NSString *key = [cookieKeyValueString substringWithRange:keyRange];
+            NSString *value = [cookieKeyValueString substringFromIndex:separatorRange.location + separatorRange.length];
+
+            key = [key stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            value = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            [cookieMap setObject:value forKey:key];
+        }
+    }
+    return cookieMap;
+}
+
+- (NSDictionary *)cookieProperties:(NSString*)cookie{
+    NSDictionary *cookieMap = [self cookieMap:cookie];
+
+    NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+    for (NSString *key in [cookieMap allKeys]) {
+
+        NSString *value = [cookieMap objectForKey:key];
+        NSString *uppercaseKey = [key uppercaseString];
+
+        if ([uppercaseKey isEqualToString:@"DOMAIN"]) {
+            if (![value hasPrefix:@"."] && ![value hasPrefix:@"www"]) {
+                value = [NSString stringWithFormat:@".%@",value];
+            }
+            [cookieProperties setObject:value forKey:NSHTTPCookieDomain];
+        }else if ([uppercaseKey isEqualToString:@"VERSION"]) {
+            [cookieProperties setObject:value forKey:NSHTTPCookieVersion];
+        }else if ([uppercaseKey isEqualToString:@"MAX-AGE"]||[uppercaseKey isEqualToString:@"MAXAGE"]) {
+            [cookieProperties setObject:value forKey:NSHTTPCookieMaximumAge];
+        }else if ([uppercaseKey isEqualToString:@"PATH"]) {
+            [cookieProperties setObject:value forKey:NSHTTPCookiePath];
+        }else if([uppercaseKey isEqualToString:@"ORIGINURL"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieOriginURL];
+        }else if([uppercaseKey isEqualToString:@"PORT"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookiePort];
+        }else if([uppercaseKey isEqualToString:@"SECURE"]||[uppercaseKey isEqualToString:@"ISSECURE"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieSecure];
+        }else if([uppercaseKey isEqualToString:@"COMMENT"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieComment];
+        }else if([uppercaseKey isEqualToString:@"COMMENTURL"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieCommentURL];
+        }else if([uppercaseKey isEqualToString:@"EXPIRES"]){
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            [dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+            dateFormatter.dateFormat = @"EEE, dd-MMM-yyyy HH:mm:ss zzz";
+            [cookieProperties setObject:[dateFormatter dateFromString:value] forKey:NSHTTPCookieExpires];
+        }else if([uppercaseKey isEqualToString:@"DISCART"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieDiscard];
+        }else if([uppercaseKey isEqualToString:@"NAME"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieName];
+        }else if([uppercaseKey isEqualToString:@"VALUE"]){
+            [cookieProperties setObject:value forKey:NSHTTPCookieValue];
+        }else{
+            [cookieProperties setObject:key forKey:NSHTTPCookieName];
+            [cookieProperties setObject:value forKey:NSHTTPCookieValue];
+        }
+    }
+        if (![cookieProperties objectForKey:NSHTTPCookiePath]) {
+        [cookieProperties setObject:@"/" forKey:NSHTTPCookiePath];
+    }
+    return cookieProperties;
+}
+
+- (NSHTTPCookie *)createCookie:(NSString*)cookie {
+  NSDictionary *cookieProperties = [self cookieProperties:cookie];
+  NSHTTPCookie *resultCookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
+  return resultCookie;
+}
+
+- (void)setCookies: (NSArray*)cookies
+{
+  NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+  NSHTTPCookieStorage *cookieStorage = [configuration HTTPCookieStorage];
+
+  int i;
+  for (i = 0; i < [cookies count]; i++) {
+   NSString cookieString = [cookies objectAtIndex:i];
+   NSHTTPCookie *cookie = [self createCookie:cookieString];
+   [cookieStorage setCookie:cookie];
+  }
+
+}
+
 - (void)startAsync:(CDVInvokedUrlCommand*)command
 {
     NSString * downloadUri = [command.arguments objectAtIndex:0];
     NSString * targetFile = [command.arguments objectAtIndex:1];
+    NSArray * cookies = [command.arguments objectAtIndex:2];
+    [self setCookies:cookies];
 
     NSString * cbid = command.callbackId;
 
